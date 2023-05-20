4d1e2b5260a27f31b44a1f425b6b20b136f5e3d6
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-20 22:10:58 +0530

    email,amount fields added

--- src/donations/pages/DonationForm.js
+++ src/donations/pages/DonationForm.js
@@ -1,49 +1,68 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { Client as Appwrite, Databases } from 'appwrite';
 import { Server } from '../utils/config';
 
 
 const DonationForm = () => {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [amount, setAmount] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleSubmit = async (event)=>{
-      event.preventDefault();
-      try{
-        const appwrite = new Appwrite();
-        const database = new Databases(appwrite);
-        appwrite.setEndpoint(Server.endpoint).setProject(Server.project);
-  
-        const data = {
-          title,
-          content
-        };
-  
-        const response = await database.createDocument("64689fe1bca86b952f51", "6468a0342d1d8955e8c3", "unique()", data);
-        console.log("Document created:");
-        if(response){
-          alert("Post Created Successfully!")
-          window.location.replace('/createDonation');
-        }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const appwrite = new Appwrite();
+      const database = new Databases(appwrite);
+      appwrite.setEndpoint(Server.endpoint).setProject(Server.project);
 
-      } catch(error){
-        console.error("Error in creating Document:",error);
-      }
-    } 
+      const data = {
+        title,
+        content,
+        amount,
+        email,
+      };
 
-    return (
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>Title:</label>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-          </div>
-          <div>
-            <label>Description:</label>
-            <input type="text" value={content} onChange={(e) => setContent(e.target.value)} />
-          </div>
-          <button type="submit">Create</button>
-        </form>
+      const response = await database.createDocument(
+        '64689fe1bca86b952f51',
+        '6468a0342d1d8955e8c3',
+        'unique()',
+        data
       );
+      console.log(response);
+
+      console.log("Document created:");
+      if (response) {
+        alert("Post Created Successfully!")
+        window.location.replace('/donations');
+      }
+
+    } catch (error) {
+      console.error("Error in creating Document:", error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Title:</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      </div>
+      <div>
+        <label>Description:</label>
+        <input type="text" value={content} onChange={(e) => setContent(e.target.value)} />
+      </div>
+      <div>
+        <label>Amount:</label>
+        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <button type="submit">Create Post</button>
+    </form>
+  );
 }
 
 export default DonationForm;
