10e28b5ce2a6e13de73c0de9a98e83510b6e8674
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-21 01:49:07 +0530

    css to donation form

--- src/donations/pages/DonationForm.js
+++ src/donations/pages/DonationForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Client as Appwrite, Databases } from 'appwrite';
 import { Server } from '../utils/config';
-
+import './DonationForm.css';
 
 const DonationForm = () => {
   const [title, setTitle] = useState("");
@@ -47,34 +47,41 @@ const DonationForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Title:</label>
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </div>
-      <div>
-        <label>Description:</label>
-        <input type="text" value={content} onChange={(e) => setContent(e.target.value)} />
-      </div>
-      <div>
-        <label>Target Amount:</label>
-        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-        <label>Phone:</label>
-        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      </div>
-      <div>
-        <label>Date:</label>
-        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-      </div>
-      
-      <button type="submit">Create Post</button>
-    </form>
+    <div className='body'>
+    <div align='center'>
+            <h1>Create a Donation</h1>
+    </div>
+    <div className="donation-form-container">
+      <br/>
+      <form onSubmit={handleSubmit} className="donation-form">
+        <div className="form-group">
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="content">Description:</label>
+          <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Target Amount:</label>
+          <input type="number" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone:</label>
+          <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="date">Date:</label>
+          <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} />
+        </div>      
+        <button type="submit" className="submit-button">Create Post</button>
+      </form>
+    </div>
+    </div>
   );
 }
 
