a438460829b61fd2c11a783d521ff853bdfb1910
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-05-21 02:44:48 +0530

    added css

--- src/donations/pages/DonationForm.js
+++ src/donations/pages/DonationForm.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Client as Appwrite, Databases } from 'appwrite';
 import { Server } from '../utils/config';
 import './Donation.css';
+import { AiOutlineMail, AiOutlinePhone, AiOutlineCalendar, AiOutlineEnvironment } from 'react-icons/ai';
+import { FaMoneyBillWave } from 'react-icons/fa';
+import { IoIosCreate } from 'react-icons/io';
 
 const DonationForm = () => {
   const [title, setTitle] = useState("");
@@ -10,6 +13,7 @@ const DonationForm = () => {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [date, setDate] = useState("");
+  const [location,setLocation] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -25,6 +29,7 @@ const DonationForm = () => {
         email,
         phone,
         date,
+        location,
       };
 
       const response = await database.createDocument(
@@ -49,35 +54,39 @@ const DonationForm = () => {
   return (
     <div className='body'>
     <div align='center'>
-            <h1>Create a Donation</h1>
+            <h1><FaMoneyBillWave className="money-icon" />  Create a Donation<hr/></h1>
     </div>
     <div className="donation-form-container">
       <br/>
       <form onSubmit={handleSubmit} className="donation-form">
         <div className="form-group">
-          <label htmlFor="title">Title:</label>
+          <label htmlFor="title"><IoIosCreate />  Title:</label>
           <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
         </div>
         <div className="form-group">
-          <label htmlFor="content">Description:</label>
+          <label htmlFor="content"><IoIosCreate /> Description:</label>
           <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} />
         </div>
         <div className="form-group">
-          <label htmlFor="amount">Target Amount:</label>
+          <label htmlFor="amount"><IoIosCreate /> Target Amount:</label>
           <input type="number" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
         </div>
         <div className="form-group">
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email"><AiOutlineMail /> Email:</label>
           <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="form-group">
-          <label htmlFor="phone">Phone:</label>
+          <label htmlFor="phone"><AiOutlinePhone /> Phone:</label>
           <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
         </div>
         <div className="form-group">
-          <label htmlFor="date">Date:</label>
+          <label htmlFor="date"><AiOutlineCalendar /> Date:</label>
           <input type="date" id="date" value={date} onChange={(e) => setDate(e.target.value)} />
-        </div>      
+        </div> 
+        <div className="form-group">
+          <label htmlFor="location"><AiOutlineEnvironment /> Location[City]:</label>
+          <input type="text" id="location" value={location} onChange={(e) => setLocation(e.target.value)} />
+        </div>     
         <button type="submit" className="submit-button">Create Post</button>
       </form>
     </div>
