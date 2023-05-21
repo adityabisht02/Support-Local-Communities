164b4165be9f267a1f4b4d59791e69bb17bb7377
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-05-21 14:18:31 +0530

    footer social icons added

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,27 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2023 Support Local Communities. All rights reserved.</p>
+      <div className="footer-content">
+        <div className="footer-social">
+          <h3>Follow Us</h3>
+          <div className="social-icons">
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+              <FaFacebook size={24} />
+            </a>
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+              <FaTwitter size={24} />
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              <FaInstagram size={24} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <p className="footer-text">© 2023 Support Local Communities. All rights reserved.</p>
     </footer>
   );
 };
