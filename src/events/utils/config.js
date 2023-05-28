47e3952e482865fd8b772175c0c5449cd65abf6a
Suruchi Kumari <suruchikumarimfp4@gmail.com>
2023-05-28 13:03:34 +0530

    Added all env variables required in our application

--- src/events/utils/config.js
+++ src/events/utils/config.js
@@ -1,7 +1,7 @@
 export const Server = {
   endpoint: process.env.REACT_APP_ENDPOINT,
   project: process.env.REACT_APP_PROJECT,
-  collectionID: process.env.REACT_APP_COLLECTION_ID,
+  collectionID: process.env.REACT_APP_EVENT_COLLECTION_ID,
   databaseID: process.env.REACT_APP_DATABASE_ID,
   trackUserLocationFunctionID:
     process.env.REACT_APP_TRACK_USER_LOCATION_FUNCTION_ID,
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,11 @@
 export const Server = {
   endpoint: process.env.REACT_APP_ENDPOINT,
   project: process.env.REACT_APP_PROJECT,
-  eventscollectionID: process.env.REACT_APP_COLLECTION_ID,
+  eventscollectionID: process.env.REACT_APP_EVENT_COLLECTION_ID,
   artcollectionID: process.env.REACT_APP_ART_COLLECTION_ID,
+  donationcollectionId: process.env.REACT_APP_DONATION_COLLECTION_ID,
   databaseID: process.env.REACT_APP_DATABASE_ID,
   bucketID: process.env.REACT_APP_STORAGE_BUCKET_ID,
-  ipgeoAPIKEY: process.env.IPGEO_API_KEY,
+  trackUserLocationFunctionID:
+    process.env.REACT_APP_TRACK_USER_LOCATION_FUNCTION_ID,
 };
