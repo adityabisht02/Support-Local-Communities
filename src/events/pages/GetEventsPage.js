3fe49ca23e2fbb778202b667be4dcea5f8a48987
Suruchi Kumari <suruchikumarimfp4@gmail.com>
2023-06-05 17:12:52 +0530

    Added cards designs in #events

--- src/events/pages/GetEventsPage.js
+++ src/events/pages/GetEventsPage.js
@@ -21,23 +21,40 @@ const GetEvents = () => {
   if (documents.length > 0) {
     return (
       <React.Fragment>
-        <section className="cards">
+        <div class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {documents.map((document) => (
-            <div className="card">
-              <img src={document.Images} alt="Event-image" />
-              <h1>{document.Name}</h1>
-              <p className="price">
-                {document.Region}, {document.City}
-              </p>
-              <p>{document.Organizers}</p>
-              <p>
+            <div class="ml-6 mt-5 mb-12 mr-6">
+              <div
+                class="relative overflow-hidden bg-cover bg-no-repeat"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                <img class="rounded-t-lg" src={document.Images} alt="" />
                 <a href={`/event/${document.$id}`}>
-                  <button>Read More</button>
+                  <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
                 </a>
-              </p>
+              </div>
+              <div class="p-6 bg-gray-800 ">
+                <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                  {document.Name}
+                </h5>
+                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                  {document.Region}, {document.City}
+                </p>
+                <a href={`/event/${document.$id}`}>
+                  <button
+                    type="button"
+                    class="inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white  transition duration-150 ease-in-out hover:bg-blue-800 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                  >
+                    Read More
+                  </button>
+                </a>
+              </div>
             </div>
           ))}
-        </section>
+        </div>
       </React.Fragment>
     );
   } else {
